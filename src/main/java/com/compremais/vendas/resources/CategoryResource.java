2256a9dcc8f1f6245344e7f9849785121b252c91
Piettro Torres <piettro.alessandro@gmail.com>
2022-09-16 14:54:10 -0300

    Teste category resource

--- src/main/java/com/compremais/vendas/resources/CategoryResource.java
+++ src/main/java/com/compremais/vendas/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.compremais.vendas.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.compremais.vendas.domain.Category;
 
 @RestController
 @RequestMapping(value = "/category")
 public class CategoryResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "REST";
+	public List<Category> list() {
+		
+		Category c1 =  new Category(1, "Informatica");
+		Category c2 =  new Category(2, "Escritório");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(c1);
+		list.add(c2);
+		return list;
+		
 	}
 	
 }
